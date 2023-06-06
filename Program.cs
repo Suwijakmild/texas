55cb5b17d67a61606a85871464addf46189985fc
Suwijakmild <123538488+Suwijakmild@users.noreply.github.com>
2023-06-06 15:02:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of tiles in width (w): ");
+        int w = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of tiles in height (h): ");
+        int h = int.Parse(Console.ReadLine());
+
+        int totalTiles = w * h;
+
+        if (totalTiles >= 26)
+        {
+            Console.WriteLine("Too stuffy!");
+        }
+        else
+        {
+            Console.WriteLine("Okay!");
+        }
+    }
+}
